2ebdc1fcdd6ceb170bfaee4d224c5b8abc8a59f3
Cyrus Omar <cyology@gmail.com>
2016-01-26 20:29:18 -0500

    fixed build file

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 ocamlbuild -use-ocamlfind \
   -pkg js_of_ocaml \
-  -pkg tyxml -pkg \
-  js_of_ocaml.tyxml hello.byte ;
+  -pkg tyxml \
+  -pkg js_of_ocaml.tyxml \
+  hello.byte ;
 
 js_of_ocaml +weak.js -o hello.js hello.byte
